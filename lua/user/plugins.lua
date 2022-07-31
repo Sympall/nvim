954f013748ce570e6917f9f0fb7a03b6dad9d103
gourav.pandey <drkknit@gmail.com>
2022-07-31 20:52:07 +0530

    onedark theme

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -62,6 +62,7 @@ return packer.startup(function(use)
 	-- Colorschemes
 	use({ "folke/tokyonight.nvim", commit = "8223c970677e4d88c9b6b6d81bda23daf11062bb" })
 	use("lunarvim/darkplus.nvim")
+  use("joshdick/onedark.vim")
 
 	-- cmp plugins
 	use({ "hrsh7th/nvim-cmp", commit = "df6734aa018d6feb4d76ba6bda94b1aeac2b378a" }) -- The completion plugin
@@ -107,7 +108,7 @@ return packer.startup(function(use)
   use "ghillb/cybu.nvim"
 
    -- Icon
-  use "kyazdani42/nvim-web-devicons" 
+  use "kyazdani42/nvim-web-devicons"
 
   -- Code Runner
   use "is0n/jaq-nvim"
